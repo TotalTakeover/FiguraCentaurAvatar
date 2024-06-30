f1eb335fbbb013b8697b0cdb0c2e28100dbd7ca2
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-06-30 14:42:14 -0700

    Update ColorProperties.lua
    - Colors for the action wheel are now host only

--- scripts/ColorProperties.lua
+++ scripts/ColorProperties.lua
@@ -3,6 +3,7 @@ local centaurParts = require("lib.GroupIndex")(models.models.Centaur)
 
 -- Variables
 local sum, count = vectors.vec3(), 0
+local average = vectors.vec3()
 local prevType, prevPrimary
 
 -- Add up primary colors
@@ -17,14 +18,6 @@ end
 local function findAverage(s, c)
 	return c ~= 0 and s / c or vec(1, 1, 1)
 end
-
--- Table setup
-local t = {}
-
-t.hover     = vectors.vec3()
-t.active    = vectors.vec3()
-t.primary   = "#"..vectors.rgbToHex(vectors.vec3())
-t.secondary = "#"..vectors.rgbToHex(vectors.vec3())
 
 function events.TICK()
 	
@@ -46,21 +39,36 @@ function events.TICK()
 	end
 	
 	-- Get Averages
-	local average = findAverage(sum, count)
+	average = findAverage(sum, count)
 	
 	prevType, prevPrimary = centaurParts.Main.Body:getPrimaryTexture()
-	
-	-- Set colors
-	t.hover     = average
-	t.active    = (average + 0.25):applyFunc(function(a) return math.min(a, 1) end)
-	t.primary   = "#"..vectors.rgbToHex(average)
-	t.secondary = "#"..vectors.rgbToHex((average + 0.25):applyFunc(function(a) return math.min(a, 1) end))
 	
 	-- Glowing outline
 	renderer:outlineColor(average)
 	
 	-- Avatar color
 	avatar:color(average)
+	
+end
+
+-- Host only instructions
+if not host:isHost() then return end
+
+-- Table setup
+local t = {}
+
+t.hover     = vectors.vec3()
+t.active    = vectors.vec3()
+t.primary   = "#"..vectors.rgbToHex(vectors.vec3())
+t.secondary = "#"..vectors.rgbToHex(vectors.vec3())
+
+function events.TICK()
+	
+	-- Set colors
+	t.hover     = average
+	t.active    = (average + 0.25):applyFunc(function(a) return math.min(a, 1) end)
+	t.primary   = "#"..vectors.rgbToHex(average)
+	t.secondary = "#"..vectors.rgbToHex((average + 0.25):applyFunc(function(a) return math.min(a, 1) end))
 	
 end
 
