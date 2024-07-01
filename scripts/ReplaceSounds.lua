a6b1fcbf7182bd67b22543b8249fdc0cb91febd2
Total <39064108+TotalTakeover@users.noreply.github.com>
2024-07-01 04:15:25 -0700

    Update ReplaceSounds.lua
    - Moved function

--- scripts/ReplaceSounds.lua
+++ scripts/ReplaceSounds.lua
@@ -46,6 +46,26 @@ local function inBox(pos, box_min, box_max)
 		   pos.z >= box_min.z and pos.z <= box_max.z
 end
 
+function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
+	
+	-- Don't trigger if the sound was played by Figura (prevent potential infinite loop)
+	if not path then return end
+	
+	-- Don't do anything if the user isn't loaded
+	if not player:isLoaded() then return end
+	
+	-- Make sure the sound is (most likely) played by the user
+	if (player:getPos() - pos):length() > 0.05 then return end
+	
+	-- If sound contains ".step", stop the sound
+	if id:find(".step") then
+		
+		return true
+		
+	end
+	
+end
+
 function events.TICK()
 	
 	-- Variables
@@ -134,25 +154,5 @@ function events.TICK()
 	
 	-- Store last ground
 	wasGround = onGround
-	
-end
-
-function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
-	
-	-- Don't trigger if the sound was played by Figura (prevent potential infinite loop)
-	if not path then return end
-	
-	-- Don't do anything if the user isn't loaded
-	if not player:isLoaded() then return end
-	
-	-- Make sure the sound is (most likely) played by the user
-	if (player:getPos() - pos):length() > 0.05 then return end
-	
-	-- If sound contains ".step", stop the sound
-	if id:find(".step") then
-		
-		return true
-		
-	end
 	
 end
