d2d68563986931b3b5f1bd6bee2ae78a5ac8a4ec
Total <39064108+TotalTakeover@users.noreply.github.com>
2025-11-11 13:39:30 -0700

    Moved spyglass functions
    Why are these not in the same event?

--- scripts/Anims.lua
+++ scripts/Anims.lua
@@ -131,6 +131,12 @@ function events.RENDER(delta, context)
 	parts.group.UpperBody:offsetPivot(crouchPos):pos(-crouchPos.x_z + crouchPos._y_ * 2)
 	parts.group.LowerBody:pos(crouchPos)
 	
+	-- Spyglass rotations
+	local headRot = vanilla_model.HEAD:getOriginRot()
+	headRot.x = math.clamp(headRot.x, -90, 30)
+	parts.group.Spyglass:offsetRot(headRot)
+		:pos(pose.crouch and vec(0, -4, 0) or nil)
+	
 end
 
 -- GS Blending Setup
@@ -147,16 +153,6 @@ for _, blend in ipairs(blendAnims) do
 	if blend.anim ~= nil then
 		blend.anim:blendTime(table.unpack(blend.ticks)):blendCurve("easeOutQuad")
 	end
-end
-
--- Fixing spyglass jank
-function events.RENDER(delta, context)
-	
-	local rot = vanilla_model.HEAD:getOriginRot()
-	rot.x = math.clamp(rot.x, -90, 30)
-	parts.group.Spyglass:offsetRot(rot)
-		:pos(pose.crouch and vec(0, -4, 0) or nil)
-	
 end
 
 -- Play sit anim
